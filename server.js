b46bd579017afafe9f3ff00ce3db8d3107dbc3ce
Chat Dude <chatdude1981@gmail.com>
2026-03-27 21:55:46 -0500

    Add online users and private messaging

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.use(express.static(__dirname));
 
 const users = {};
 
+function getUsersInRoom(room) {
+  return Object.entries(users)
+    .filter(([_, user]) => user.room === room)
+    .map(([socketId, user]) => ({
+      socketId,
+      username: user.username
+    }));
+}
+
+function updateUserList(room) {
+  io.to(room).emit("user list", getUsersInRoom(room));
+}
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
@@ -24,6 +37,7 @@ io.on("connection", (socket) => {
     users[socket.id] = { username, room };
 
     socket.to(room).emit("system message", `${username} joined the room`);
+    updateUserList(room);
   });
 
   socket.on("chat message", (msg) => {
@@ -36,11 +50,29 @@ io.on("connection", (socket) => {
     });
   });
 
+  socket.on("private message", ({ toSocketId, message }) => {
+    const fromUser = users[socket.id];
+    const toUser = users[toSocketId];
+
+    if (!fromUser || !toUser || !message.trim()) return;
+
+    io.to(toSocketId).emit("private message", {
+      from: fromUser.username,
+      message
+    });
+
+    socket.emit("private message", {
+      from: `(to ${toUser.username})`,
+      message
+    });
+  });
+
   socket.on("disconnect", () => {
     const user = users[socket.id];
     if (user) {
       socket.to(user.room).emit("system message", `${user.username} left the room`);
       delete users[socket.id];
+      updateUserList(user.room);
     }
 
     console.log("User disconnected:", socket.id);
