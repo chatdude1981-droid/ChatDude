173355d6b517c314fccf2bd90caefbbb5b042628
Chat Dude <chatdude1981@gmail.com>
2026-03-27 20:40:51 -0500

    Add usernames and join leave messages

--- server.js
+++ server.js
@@ -14,14 +14,32 @@ const io = new Server(server, {
 
 app.use(express.static(__dirname));
 
+const users = {};
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  socket.on("set username", (username) => {
+    users[socket.id] = username;
+
+    io.emit("system message", `${username} joined the chat`);
+  });
+
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    const username = users[socket.id] || "Anonymous";
+    io.emit("chat message", {
+      username,
+      message: msg
+    });
   });
 
   socket.on("disconnect", () => {
+    const username = users[socket.id];
+    if (username) {
+      io.emit("system message", `${username} left the chat`);
+      delete users[socket.id];
+    }
+
     console.log("User disconnected:", socket.id);
   });
 });
